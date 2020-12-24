df0aa52b65ee7c7cea3534b3522a499e230413fa
VENKATA RAMIREDDY KASU <ram.kasu@gmail.com>
2020-12-24 19:11:03 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-function sayHello(){
-	return "Hi, Hellow World";
+/*!
+ * login-utility
+ * Copyright(c) 2020 Kasu Venkata Ramireddy <ram.kasu@gmail.com>
+ * MIT Licensed
+ */
+
+'use strict'
+
+
+/**
+ * Private variables
+ */
+
+
+ class LoginActivity{
+
+	constructor(request){
+		this.request=request
+	}
+
+
+	async getIP(){
+		return await this.request.headers['cf-connecting-ip'] || this.request.headers['x-forwarded-for'] || this.request.connection.remoteAddress
+	}
+
 }
 
-module.exports= sayHello;
+module.exports = LoginActivity
+
+
